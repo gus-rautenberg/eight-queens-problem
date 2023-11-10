7394f0aa4dfdaee935d3ef0a7f27b2533abae289
gus-rautenberg <gprautenber@gmail.com>
2023-11-10 09:45:18 -0300

    Board

--- board.cpp
+++ board.cpp
@@ -3,18 +3,20 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
 Board::Board() {
     size = 8; // Tamanho padrão do xadrez
-    board.resize(size, 0);
+    posX.resize(size, 0);
+    posY.resize(size, 0);
 }
 
 void Board::printBoard() const {
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            if (alguma coisa pra dizer que rainha ta aqui) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (posX[i] == j) {
                 cout << "Q ";
             } else {
                 cout << ". ";
@@ -29,10 +31,22 @@ int Board::calculateCost() const {
     int cost = 0;
     for (int i = 0; i < size; ++i) {
         for (int j = i + 1; j < size; ++j) {
-            if (board[i] == board[j] || abs(board[i] - board[j]) == j - i) {
+            if (posX[i] == posX[j] || abs(posX[i] - posX[j]) == abs(posY[i] - posY[j])) {
                 ++cost;
             }
         }
     }
     return cost;
 }
+
+void Board::initialRandomBoard() {
+    for (int i = 0; i < size; ++i) {
+        posX[i] = i;
+        posY[i] = i;
+    }
+
+    // Embaralha aleatoriamente os vetores
+    random_shuffle(posX.begin(), posX.end());
+    random_shuffle(posY.begin(), posY.end());
+}
+
