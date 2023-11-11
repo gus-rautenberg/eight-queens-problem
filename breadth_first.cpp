b2f0dc22e5b3e36c2951c43bc234501ca16d0559
Joao-Biederman <joaobiedetman@gmail.com>
2023-11-11 10:12:03 -0300

    Creating a board for each branch and semaphore

--- breadth_first.cpp
+++ breadth_first.cpp
@@ -2,23 +2,43 @@
 #include <iostream>
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>
 
 using namespace std;
 
-
+sem_t semaphore;
 
 void first_breadth() {
-    Board board;
+    Board board[5];
+    pthread_t new_thread[5];
 
-    pthread_t new_thread;
-    int result = pthread_create(&new_thread, NULL, breath_first, &board);
-    pthread_join(new_thread, NULL);
+    sem_init(&semaphore, 0, 1);
+
+    for (int i = 0; i < 5; i++)
+    {
+        board[i].initialRandomBoard();
+        pthread_create(&new_thread[i], NULL, breath_first, &board[i]);
+    }
+    
+    for (int i = 0; i < 5; i++)
+    {
+        pthread_join(new_thread[i], NULL);   
+    }
+
+    
+    sem_wait(&semaphore);
+    cout << "return" << endl;
+    sem_post(&semaphore);
+
+    sem_destroy(&semaphore);
+    return;
 }
 
 void* breath_first(void* arg) {
     
     Board* board = static_cast<Board*>(arg);
-
-    cout << "1" << endl;
+    sem_wait(&semaphore);
+    board->printBoard();
+    sem_post(&semaphore);
     return NULL;
 }
