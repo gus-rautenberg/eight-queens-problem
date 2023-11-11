4c75a8688d2a0e96e33eee346c6b1c5daa62782c
Joao-Biederman <joaobiedetman@gmail.com>
2023-11-11 10:53:08 -0300

    Updates breath_first

--- breadth_first.cpp
+++ breadth_first.cpp
@@ -9,18 +9,18 @@ using namespace std;
 sem_t semaphore;
 
 void first_breadth() {
-    Board board[5];
-    pthread_t new_thread[5];
+    Board board[8];
+    pthread_t new_thread[8];
 
     sem_init(&semaphore, 0, 1);
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 8; i++)
     {
         board[i].initialRandomBoard();
         pthread_create(&new_thread[i], NULL, breath_first, &board[i]);
     }
     
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 8; i++)
     {
         pthread_join(new_thread[i], NULL);   
     }
@@ -37,8 +37,21 @@ void first_breadth() {
 void* breath_first(void* arg) {
     
     Board* board = static_cast<Board*>(arg);
+    
+    if ()
+    {
+        /* code */
+    }
+    
+
+    for (int i = 0; i < 8; i++)
+    {
+        
+    }
+    
     sem_wait(&semaphore);
     board->printBoard();
+    cout << endl;
     sem_post(&semaphore);
     return NULL;
 }
