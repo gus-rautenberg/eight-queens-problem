31848e29099fb2183b3bc8d9c12186d88907b2a2
Augusto Barella Dal Pra <augusto_dalpra@hotmail.com>
2023-11-10 23:08:17 -0300

    Board.h with adaptations
    Mudei a .cpp, mudei a .h

--- board.h
+++ board.h
@@ -3,6 +3,7 @@
 #define BOARD_H
 
 #include <vector>
+
 using namespace std;
 
 class Board {
@@ -10,10 +11,22 @@ public:
     Board();
     void printBoard() const;
     int calculateCost() const;
+    void initialRandomBoard();
+    Board(const Board &board);
+    void randomMove();
+    bool isPositionFree(int x, int y) const;
+    vector<int> getVectorX() const;
+    vector<int> getVectorY() const;
+    void copyBoard(const Board &source);
+    void setVectorX(const vector<int>& newVectorX);
+    void setVectorY(const vector<int>& newVectorY);
+
 
 private:
     int size;
-    vector<int> board;
+    vector<int> posX; // Vetor para armazenar as posições X das rainhas
+    vector<int> posY; // Vetor para armazenar as posições Y das rainhas
+    void moveQueen(int queenIndex, int newPositionX, int newPositionY);
 };
 
 #endif // BOARD_H
