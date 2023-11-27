30de69d35b4c79114a5cd6ad7dab4f738b4c7ef9
Joao-Biederman <joaobiedetman@gmail.com>
2023-11-27 10:37:38 -0300

    git merge main -> rautenberg

--- board.h
+++ board.h
@@ -9,7 +9,7 @@ using namespace std;
 class Board {
 public:
     Board();
-    void printBoard(const string& filename) const;
+    void printBoard() const;
     int calculateCost() const;
     void initialRandomBoard();
     Board(const Board &board);
@@ -17,19 +17,23 @@ public:
     bool isPositionFree(int x, int y) const;
     vector<int> getVectorX() const;
     vector<int> getVectorY() const;
+    int getSize();
     void copyBoard(const Board &source);
     void setVectorX(const vector<int>& newVectorX);
     void setVectorY(const vector<int>& newVectorY);
     int getQueensQuantity();
-    void setQueensQuantity(int x);
-    void placeQueen(int x, int y);
+    void addQueen();
+    void placeQueen(int queenIndex, int x, int y);
+    int isAttacking(int newestQueen);
+    void moveQueen(int queenIndex, int newPositionX, int newPositionY);
+
+    void printPos();
 
 private:
     int size;
     int queensQuantity;
     vector<int> posX; // Vetor para armazenar as posições X das rainhas
     vector<int> posY; // Vetor para armazenar as posições Y das rainhas
-    void moveQueen(int queenIndex, int newPositionX, int newPositionY);
 };
 
 #endif // BOARD_H
