356d48af5809806f8ba704d52f78bd4e133c9759
gus-rautenberg <gprautenber@gmail.com>
2023-11-28 16:23:54 -0300

    Trying to create an interface

---
+++ teste.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+int main() {
+    const string outputFileName = "output.json";
+    int posX[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+    int posY[8] = {8, 7, 6, 5, 4, 3, 2, 1};
+    // Obtém um gerador de números aleatórios
+    random_device rd;
+    mt19937 g(rd());
+    ofstream outputFile(outputFileName);
+    outputFile << "{";
+    int total = 50;
+    for (int a = 0; a < total; a++) {
+        // Embaralha os arrays
+        shuffle(begin(posX), end(posX), g);
+        shuffle(begin(posY), end(posY), g);
+
+        if (outputFile.is_open()) {
+            outputFile << " \"queens" << a << "\": [";
+
+            for (int i = 0; i < 8; ++i) {
+                outputFile << "{ \"x\": " << posX[i] << ", \"y\": " << posY[i] << "}";
+                if (i < 7) {
+                    outputFile << ",";
+                }
+            }
+
+            outputFile << "]";
+            if(a < total-1){
+                outputFile << ", ";
+            }
+            
+
+            
+        } 
+    }
+    outputFile << "} ";
+    outputFile.close();
+        
+    return 0;
+}
